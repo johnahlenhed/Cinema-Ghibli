087b9f9d960d2475e8db8480eeb06a736d656781
Otloir <oli.toreland@gmail.com>
2025-11-06 13:41:09 +0100

    Created a data.php file along with two arrays
    Created a data.php file along with two arrays. One array for current movies and the other for upcoming movies.

---
+++ assets/data.php
@@ -0,0 +1,150 @@
+<?php 
+
+$current_movies = [
+    "Spirited Away" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/d/db/Spirited_Away_Japanese_poster.png",
+        "jp-title" => "千と千尋の神隠し",
+        "genre" => [
+            "Adventure",
+            "Family"
+        ],
+    ],
+    "Howl's Moving Castle" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/a/a0/Howls-moving-castleposter.jpg",
+        "jp-title" => "ハウルの動く城",
+        "genre" => [
+            "Romance",
+            "Slice of life"
+        ],
+    ],
+    "Porco Rosso" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/f/fc/Porco_Rosso_%28Movie_Poster%29.jpg",
+        "jp-title" => "紅の豚",
+        "genre" => [
+            "Fantasy",
+            "Comedy"
+        ],
+    ],
+    "My Neighbor Totoro" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/0/02/My_Neighbor_Totoro_-_Tonari_no_Totoro_%28Movie_Poster%29.jpg",
+        "jp-title" => "となりのトトロ",
+        "genre" => [
+            "Family",
+            "Adventure"
+        ],
+    ],
+    "Pom Poko" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/6/68/Pompokoposter.jpg",
+        "jp-title" => "平成狸合戦ぽんぽこ",
+        "genre" => [
+            "Romance",
+            "Slice of life"
+        ],
+    ],
+    "Kiki's Delivery Service" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/0/07/Kiki%27s_Delivery_Service_%28Movie%29.jpg",
+        "jp-title" => "魔女の宅急便",
+        "genre" => [
+            "Family",
+            "Fantasy"
+        ],
+    ],
+    "Grave of the Fireflies" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/a/a5/Grave_of_the_Fireflies_Japanese_poster.jpg",
+        "jp-title" => "火垂るの墓",
+        "genre" => [
+            "Tradgedy",
+            "Drama"
+        ],
+    ],
+    "Castle in the Sky" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/f/f5/Castle_in_the_Sky_%281986%29.png",
+        "jp-title" => "天空の城ラピュタ",
+        "genre" => [
+            "Adventure",
+            "Fantasy"
+        ],
+    ],
+    "Princess Mononoke" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/8/8c/Princess_Mononoke_Japanese_poster.png",
+        "jp-title" => "もののけ姫",
+        "genre" => [
+            "Dark Fantasy",
+            "Action"
+        ],
+    ],
+    "Ponyo" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/5/51/Ponyo.png",
+        "jp-title" => "崖の上のポニョ",
+        "genre" => [
+            "Comedy",
+            "Family"
+        ],
+    ],
+    "The Secret World of Arrietty" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/e/e7/Karigurashi_no_Arrietty_poster.png",
+        "jp-title" => "借りぐらしのアリエッティ",
+        "genre" => [
+            "Family",
+            "Drama"
+        ],
+    ],
+    "Tales from Earthsea" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/e/e5/Gedo6sn.jpg",
+        "jp-title" => "ゲド戦記",
+        "genre" => [
+            "Adventure",
+            "Fantasy"
+        ],
+    ],
+];
+
+$upcoming_movies = [
+    "From Up on Poppy Hill" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/c/c9/From_Up_on_Poppy_Hill.png",
+        "jp-title" => "コクリコ坂から",
+        "genre" => [
+            "Romance",
+            "Slice of life"
+        ],
+    ],
+    "The Wind Rises" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/a/a3/Kaze_Tachinu_poster.jpg",
+        "jp-title" => "風立ちぬ",
+        "genre" => [
+            "Romance",
+            "Slice of life"
+        ],
+    ],
+    "The Tale of the Princess Kaguya" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/6/68/The_Tale_of_the_Princess_Kaguya_%28poster%29.jpg",
+        "jp-title" => "かぐや姫の物語",
+        "genre" => [
+            "Comedy"
+        ],
+    ],
+    "When Marnie Was There" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/a/a7/When_Marnie_Was_There.png",
+        "jp-title" => "思い出のマーニー",
+        "genre" => [
+            "Drama",
+            "Mystery"
+        ],
+    ],
+    "The red turtle" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/f/fe/The_Red_Turtle.png",
+        "jp-title" => " ",
+        "genre" => [
+            "Drama",
+            "Slice of life"
+        ],
+    ],
+    "The Boy and the Heron" => [
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/4/41/How_Do_You_Live_poster.jpg",
+        "jp-title" => "君たちはどう生きるか",
+        "genre" => [
+            "Isekai",
+            "Adventure"
+        ],
+    ],
+];
