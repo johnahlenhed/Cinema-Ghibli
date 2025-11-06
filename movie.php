c7fdf0d53ca718b679840e042a8653d313a6eca8
Otloir <oli.toreland@gmail.com>
2025-11-06 21:25:39 +0100

    added text

--- movie.php
+++ movie.php
@@ -1,7 +1,120 @@
 <?php include __DIR__ . '/includes/header.php'; ?>
-
+<head>
+    <link rel="stylesheet" href="/assets/movie.css">
+</head>
 <main>
-    <h1>Movie Details</h1>
+    <section>
+        <h1>Spirited Away</h1>
+        <span>7+</span>
+        <span>Adventure, Family</span>
+    </section>
+    <section class="trailer">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ByXuk9QqQkk?si=GbP3280hGcEmbPSS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </section>
+    <section class="synopsis">
+        <h2>Synopsis:</h2>
+        <p>When you think you've lost your way, you may have just found your way. </p>
+        <p>10-year-old Chihiro is on her way to a new home with her parents when they 
+            accidentally take a detour - straight into a world unlike anything she has 
+            known before. 
+            When night falls, the place transforms into a bathhouse for spirits and supernatural 
+            beings. Her parents have turned into pigs, and Chihiro is suddenly alone in a world 
+            where the rules are incomprehensible and danger hides in the shadows. 
+            But in the middle of this enchanted world, she meets Haku - an enigmatic 
+            boy who becomes her ally. He warns her: if she forgets her name, she also forgets who 
+            she is - and then she is trapped in the spirit world forever. 
+            To survive and find her way back home, Chihiro must find the courage to stand up for 
+            herself, and preserve the memory of who she once was.</p>
+    </section>
+    <br>
+    <section class="cast">
+        <h2>Cast:</h2>
+        <div>
+            <p>Director/Writer:</p>
+            <p>Hayao Miyazaki</p>
+
+            <p>Producer:</p>
+            <p>Toshio Suzuki</p>
+
+            <p>Music:</p>
+            <p>Joe Hisaishi</p>
+
+            <br>
+
+            <p>Japanese voices:</p>
+
+            <p>Rumi Hiiragi as Chihiro</p>
+
+            <p>Miyu Irino as Haku</p>
+
+            <p>Mari Natsuki as Yubāba/Zeniiba</p>
+        </div>
+        <div>
+            <p>Original language: </p>
+            <p>Japanese</p>
+
+            <p>Runtime:</p>
+            <p>2h 5m</p>
+
+            <p>Release year:</p>
+            <p>20 July 2001</p>
+
+            <br>
+
+            <p>English voices:</p>
+
+            <p>Daveigh Chase as Chihiro</p>
+
+            <p>Jason Marsden as Haku</p>
+
+            <p>Suzanne Pleshette as Yubāba/Zeniiba</p>
+        </div>
+    </section>
+    <br>
+    <section class="booking">
+        <h2>Tickets:</h2>
+        <div class="container">
+            <div>
+                <p>Tuseday</p>
+                <p>7th nov</p>
+            </div>
+            <div>
+                <p>Wednesday</p>
+                <p>8th nov</p>
+            </div>
+            <div>
+                <p>Sunday</p>
+                <p>11th nov</p>
+            </div>
+        </div>
+
+        <br>
+
+        <h4>JP voice, ENG text</h4>
+        <div class="container">
+            <div>
+                <p>18:45</p>
+                <p>5th hall</p>
+                <p>30/40</p>
+            </div>
+        </div>
+
+        <br>
+
+        <h4>ENG voice</h4>
+        <div class="container">
+            <div>
+                <p>13:00</p>
+                <p>5rd hall</p>
+                <p>20/40</p>
+            </div>
+            <div>
+                <p>20:00</p>
+                <p>3rd hall</p>
+                <p>40/60</p>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php include __DIR__ . '/includes/footer.php'; ?>
