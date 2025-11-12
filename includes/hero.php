9b4ce5a2d61d988c44ecaed33cf6a13a98d63982
John Ahlenhed <johnahlenhed@gmail.com>
2025-11-12 16:25:58 +0100

    Hero functions

---
+++ includes/hero.php
@@ -0,0 +1,33 @@
+<hero class="hero">
+
+    <div class="hero-slideshow">
+        <div class="hero-slide1">
+        </div>
+        <div class="hero-slide2">
+        </div>
+    </div>
+
+    <section class="hero-overlay">
+        <div class="hero-content-container">
+            <div class="hero-logo">
+            <img src="/assets/images/cinema-ghibli-logo-light.svg" alt="">
+        </div>
+        <div class="hero-text-content">
+            <p>SET SLIDESHOW AS BG-IMAGE!</p>
+        </div>
+
+        <div class="hero-buttons">
+            <button class="hero-button-upcoming">
+                Upcoming Movies
+            </button>
+
+            <button class="hero-button-movies">
+                All Movies
+            </button>
+        </div>
+        </div>
+    </section>
+
+
+
+</hero>
