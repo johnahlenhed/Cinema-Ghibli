79773e6314e66cfed8c23d1a4de0ddde766bb7dd
John Ahlenhed <johnahlenhed@gmail.com>
2025-11-27 16:38:49 +0100

    More hero images in animation

--- includes/header.php
+++ includes/header.php
@@ -17,9 +17,11 @@
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
-
             </div>
         </nav>
+        <div class="header-offer">
+            <h5>Buy a ticket now, and get a chance to win an exclusive customized popcorn bucket!</h5>
+        </div>
     </header>
 
     <div class="mobile-menu">
