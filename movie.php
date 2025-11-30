ad7f69e0bc1935cf14284413355e1ee641aaa3c5
Otloir <oli.toreland@gmail.com>
2025-11-30 19:47:15 +0100

    updated the cta button

--- movie.php
+++ movie.php
@@ -3,160 +3,172 @@
 <head>
     <link rel="stylesheet" href="/assets/movie.css">
 </head>
-<main>
-    <section class="movieTitle">
-        <h2>Spirited Away</h2>
-        <div>
-            <span class="ageRating">7+</span>
-            <span class="movieGenre">Adventure,</span>
-            <span class="movieGenre">Family</span>
+<body>
+    <main>
+    
+        <!-- title -->
+        <section class="movieTitle">
+            <h2>Spirited Away</h2>
+            <div>
+                <span class="ageRating">7+</span>
+                <span class="movieGenre">Adventure,</span>
+                <span class="movieGenre">Family</span>
+            </div>
+        </section>
+    
+        <!-- trailer -->
+        <div class="trailer">
+            <iframe class="responsive-iframe" src="https://www.youtube.com/embed/ByXuk9QqQkk?si=qdcTF4G70ObH7q03" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
-    </section>
-    <div class="trailer">
-        <iframe class="responsive-iframe" src="https://www.youtube.com/embed/ByXuk9QqQkk?si=qdcTF4G70ObH7q03" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    </div>
-    <section class="synopsis" id="infoContainer">
-        <h3>Synopsis:</h3>
-        <span class="line"></span>
-        <p>When you think you've lost your way, you may have just found your way. </p>
-        <p>10-year-old Chihiro is on her way to a new home with her parents when they
-            accidentally take a detour - straight into a world unlike anything she has
-            known before. </p>
-        <p> When night falls, the place transforms into a bathhouse for spirits and supernatural
-            beings. Her parents have turned into pigs, and Chihiro is suddenly alone in a world
-            where the rules are incomprehensible and danger hides in the shadows.
-            But in the middle of this enchanted world, she meets Haku - an enigmatic
-            boy who becomes her ally. He warns her: if she forgets her name, she also forgets who
-            she is - and then she is trapped in the spirit world forever. </p>
-        <p> To survive and find her way back home, Chihiro must find the courage to stand up for
-            herself, and preserve the memory of who she once was.</p>
-    </section>
-    <div class="castBookingContainer">
-        <section class="cast" id="infoContainer">
-            <h3>Cast:</h3>
+    
+        <!-- synopsis -->
+        <section class="synopsis" id="infoContainer">
+            <h3>Synopsis:</h3>
             <span class="line"></span>
-            <div class="castTable">
-                <table>
-                    <tr>
-                        <th>Director/Writer:</th>
-                    </tr>
-                    <tr>
-                        <td>Hayao Miyazaki</td>
-                    </tr>
-                    <tr>
-                        <th>Producer:</th>
-                    </tr>
-                        <tr>
-                        <td>Toshio Suzuki</td>
-                    </tr>
-                        <tr>
-                        <th>Music:</th>
-                    </tr>
-                        <tr>
-                        <td>Joe Hisaishi</td>
-                    </tr>
-                        <tr>
-                        <td><br></td>
-                    </tr>
-                        <tr>
-                        <th>Japanese voices:</th>
-                    </tr>
-                        <tr>
-                        <td>Rumi Hiiragi as Chihiro</td>
-                    </tr>
-                        <tr>
-                        <td>Miyu Irino as Haku</td>
-                    </tr>
-                        <tr>
-                        <td>Mari Natsuki as Yubāba/Zeniiba</td>
-                    </tr>
-                </table>
-
-
-                <table>
-                    <tr>
-                        <th>Original language:</th>
-                    </tr>
-                    <tr>
-                        <td>Japanese</td>
-                    </tr>
-                    <tr>
-                        <th>Runtime:</th>
-                    </tr>
-                        <tr>
-                        <td>2h 5m</td>
-                    </tr>
-                        <tr>
-                        <th>Release year:</th>
-                    </tr>
-                        <tr>
-                        <td>20 July 2001</td>
-                    </tr>
-                        <tr>
-                        <td><br></td>
-                    </tr>
-                        <tr>
-                        <th>English voices:</th>
-                    </tr>
-                        <tr>
-                        <td>Daveigh Chase as Chihiro</td>
-                    </tr>
-                        <tr>
-                        <td>Jason Marsden as Haku</td>
-                    </tr>
-                        <tr>
-                        <td>Suzanne Pleshette as Yubāba/Zeniiba</td>
-                    </tr>
-                </table>
-            </div>
+            <p>When you think you've lost your way, you may have just found your way. </p>
+            <p>10-year-old Chihiro is on her way to a new home with her parents when they
+                accidentally take a detour - straight into a world unlike anything she has
+                known before. </p>
+            <p> When night falls, the place transforms into a bathhouse for spirits and supernatural
+                beings. Her parents have turned into pigs, and Chihiro is suddenly alone in a world
+                where the rules are incomprehensible and danger hides in the shadows.
+                But in the middle of this enchanted world, she meets Haku - an enigmatic
+                boy who becomes her ally. He warns her: if she forgets her name, she also forgets who
+                she is - and then she is trapped in the spirit world forever. </p>
+            <p> To survive and find her way back home, Chihiro must find the courage to stand up for
+                herself, and preserve the memory of who she once was.</p>
         </section>
-        <section class="booking" id="infoContainer">
-            <h3>Tickets:</h3>
-            <span class="line"></span>
-            <div class="date">
-                <button>
-                    <p>Tuseday</p>
-                    <p>7th nov</p>
-                </button>
-                <button>
-                    <p>Wednesday</p>
-                    <p>8th nov</p>
-                </button>
-                <button>
-                    <p>Sunday</p>
-                    <p>11th nov</p>
-                </button>
-            </div>
+        <!-- cast -->
+        <div class="castBookingContainer">
+            <section class="cast" id="infoContainer">
+                <h3>Cast:</h3>
+                <span class="line"></span>
+                <div class="castTable">
+                    <!-- left row -->
+                    <table>
+                        <tr>
+                            <th>Director/Writer:</th>
+                        </tr>
+                        <tr>
+                            <td>Hayao Miyazaki</td>
+                        </tr>
+                        <tr>
+                            <th>Producer:</th>
+                        </tr>
+                            <tr>
+                            <td>Toshio Suzuki</td>
+                        </tr>
+                            <tr>
+                            <th>Music:</th>
+                        </tr>
+                            <tr>
+                            <td>Joe Hisaishi</td>
+                        </tr>
+                            <tr>
+                            <td><br></td>
+                        </tr>
+                            <tr>
+                            <th>Japanese voices:</th>
+                        </tr>
+                            <tr>
+                            <td>Rumi Hiiragi as Chihiro</td>
+                        </tr>
+                            <tr>
+                            <td>Miyu Irino as Haku</td>
+                        </tr>
+                            <tr>
+                            <td>Mari Natsuki as Yubāba/Zeniiba</td>
+                        </tr>
+                    </table>
     
-            <br>
+                    <!-- right row -->
+                    <table>
+                        <tr>
+                            <th>Original language:</th>
+                        </tr>
+                        <tr>
+                            <td>Japanese</td>
+                        </tr>
+                        <tr>
+                            <th>Runtime:</th>
+                        </tr>
+                            <tr>
+                            <td>2h 5m</td>
+                        </tr>
+                            <tr>
+                            <th>Release year:</th>
+                        </tr>
+                            <tr>
+                            <td>20 July 2001</td>
+                        </tr>
+                            <tr>
+                            <td><br></td>
+                        </tr>
+                            <tr>
+                            <th>English voices:</th>
+                        </tr>
+                            <tr>
+                            <td>Daveigh Chase as Chihiro</td>
+                        </tr>
+                            <tr>
+                            <td>Jason Marsden as Haku</td>
+                        </tr>
+                            <tr>
+                            <td>Suzanne Pleshette as Yubāba/Zeniiba</td>
+                        </tr>
+                    </table>
+                </div>
+            </section>
     
-            <h4>JP voice, ENG text</h4>
-            <div class="time">
-                <button>
-                    <p>18:45</p>
-                    <p>5th hall</p>
-                    <p>30/40</p>
-                </button>
-            </div>
-    
-            <br>
-    
-            <h4>ENG voice</h4>
-            <div class="time">
-                <button>
-                    <p>13:00</p>
-                    <p>5rd hall</p>
-                    <p>20/40</p>
-                </button>
-                <button>
-                    <p>20:00</p>
-                    <p>3rd hall</p>
-                    <p>40/60</p>
-                </button>
-            </div>
-        </section>
-    </div>
-</main>
+            <!-- booking -->
+            <section class="booking" id="infoContainer">
+                <h3>Tickets:</h3>
+                <span class="line"></span>
+                <div class="date">
+                    <button>
+                        <p>Tuseday</p>
+                        <p>7th nov</p>
+                    </button>
+                    <button>
+                        <p>Wednesday</p>
+                        <p>8th nov</p>
+                    </button>
+                    <button>
+                        <p>Sunday</p>
+                        <p>11th nov</p>
+                    </button>
+                </div>
+        
+                <br>
+        
+                <h4>JP voice, ENG text</h4>
+                <div class="time">
+                    <button>
+                        <p>18:45</p>
+                        <p>5th hall</p>
+                        <p>30/40</p>
+                    </button>
+                </div>
+        
+                <br>
+        
+                <h4>ENG voice</h4>
+                <div class="time">
+                    <button>
+                        <p>13:00</p>
+                        <p>5rd hall</p>
+                        <p>20/40</p>
+                    </button>
+                    <button>
+                        <p>20:00</p>
+                        <p>3rd hall</p>
+                        <p>40/60</p>
+                    </button>
+                </div>
+            </section>
+        </div>
+    </main>
+</body>
 
 <script src="/assets/booking.js"></script>
 
